d6200c170170b7655778c843fe38635191670d6c
Martine <45560363+bdevillers@users.noreply.github.com>
2024-11-11 18:41:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const appVersion = 'v. 18.30-3';
+    const appVersion = 'v. 18.40';
     
     const takePhotoBtn = document.getElementById("takePhotoBtn");
     const cameraInput = document.getElementById("cameraInput");
